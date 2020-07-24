fbcff3791a74c1e11e797b501cdcaabcff831a85
Xeyal <xeyalbeylerov@gmail.com>
2020-07-25 02:42:44 +0400

    ResumeService and Controller modified but yet test

--- src/main/java/com/company/resumecv/controller/ResumeController.java
+++ src/main/java/com/company/resumecv/controller/ResumeController.java
@@ -3,6 +3,7 @@ package com.company.resumecv.controller;
 import com.company.resumecv.entity.Resume;
 import com.company.resumecv.entity.Skill;
 import com.company.resumecv.form.*;
+import com.company.resumecv.service.Html2Pdf;
 import com.company.resumecv.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
 
+import javax.websocket.server.PathParam;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +25,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ResumeController {
     private final ResumeService resumeService;
+    private final Html2Pdf generatePdf;
+
     @GetMapping("/resume")
     public String resume(Model model) {
         List<ResumeForm> resumes = resumeService.getAll();
@@ -28,26 +34,20 @@ public class ResumeController {
         return "resume";
     }
 
-    @GetMapping("/builder")
-    public String builder(Model model) {
 
-        ResumeForm resumeForm=new ResumeForm();
 
-        resumeForm.setEducations(Arrays.asList(new EducationForm()));
-        resumeForm.setSkills(Arrays.asList(new SkillForm()));
-        resumeForm.setSocials(Arrays.asList(new SocialForm()));
-        resumeForm.setWorkExperience(Arrays.asList(new WorkExperienceForm()));
-
-        model.addAttribute("resume",resumeForm );
-        return "builderTest";
+    @GetMapping("/resume/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        ResumeForm resumeForm = resumeService.findById(id);
+        model.addAttribute("resume", resumeForm);
+        return "resume";
     }
-    @PostMapping("/builder")
-    public String builderPost(Model model,@ModelAttribute ResumeForm resume) {
-        resumeService.save(resume);
 
-        ResumeForm resumeForm=new ResumeForm();
-        model.addAttribute("resume",resumeForm );
-        return "builderTest";
+    @GetMapping("/resumeTemplate/{id}")
+    public String findByIdResumeTemplate(@PathVariable("id") Long id, Model model) {
+        ResumeForm resumeForm = resumeService.findById(id);
+        model.addAttribute("resume", resumeForm);
+        return "resumeTemplate";
     }
 
     @GetMapping
@@ -56,6 +56,16 @@ public class ResumeController {
         return "index";
     }
 
+    @GetMapping("/generate/{id}")
+    public String generatePdf(@PathVariable("id") Long id, Model model) {
+        ResumeForm resumeForm = resumeService.findById(id);
+        String inputUrl ="http://localhost:8080/resume/"+id;
+        String outputUrl ="TestPdf_"+id+".pdf";
+        generatePdf.generatePDF(inputUrl);
+
+        model.addAttribute("resume", resumeForm);
+        return "resume";
+    }
     @GetMapping("/up")
     public String greetingPage(Model model, @RequestParam(required = false) String name) {
         name = name == null || name.trim().equals("") ? "Thymeleaf" : name;
@@ -67,7 +77,7 @@ public class ResumeController {
 
     @GetMapping("/testSpring")
     public String greetingForm(Model model) {
-        SkillForm skillForm=new SkillForm();
+        SkillForm skillForm = new SkillForm();
 //        model.addAttribute("skillForm", skillForm);
         model.addAttribute("skillForm", skillForm);
         return "testSpring";
@@ -77,5 +87,17 @@ public class ResumeController {
     public String greetingSubmit(@ModelAttribute SkillForm skillForm) {
         System.out.println(skillForm);
         return "testSpring";
+    }
+
+    //    Blank resume object
+    @ModelAttribute("resume")
+    public ResumeForm getEmptyUserForm() {
+        ResumeForm resumeForm = new ResumeForm();
+        resumeForm.setId(1l);
+        resumeForm.setEducations(Arrays.asList(new EducationForm()));
+        resumeForm.setSkills(Arrays.asList(new SkillForm()));
+        resumeForm.setSocials(Arrays.asList(new SocialForm()));
+        resumeForm.setWorkExperience(Arrays.asList(new WorkExperienceForm()));
+        return resumeForm;
     }
 }
--- src/main/java/com/company/resumecv/service/ResumeService.java
+++ src/main/java/com/company/resumecv/service/ResumeService.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -24,11 +25,24 @@ public class ResumeService {
         System.out.println(resume);
         return resumeForm;
     }
+    @Transactional
+    public ResumeForm update(ResumeForm resumeForm) {
+        Assert.notNull(resumeForm.getId(),"Id bos ola bilmez");
+        Resume resume = modelMapper.map(resumeForm, Resume.class);
+        resume = repository.save(resume);
+        System.out.println(resume);
+        return resumeForm;
+    }
 
     public List<ResumeForm> getAll() {
         List<Resume> resumes=repository.findAll();
 //        ModelMapper List converter
         List<ResumeForm> resumeForms=modelMapper.map(resumes,new TypeToken<List<ResumeForm>>() {}.getType());
+        return resumeForms;
+    }
+    public ResumeForm findById(Long id) {
+       Resume resume=repository.getOne(id);
+       ResumeForm resumeForms=modelMapper.map(resume,ResumeForm.class);
         return resumeForms;
     }
 }
