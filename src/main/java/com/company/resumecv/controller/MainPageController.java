272668069dc2a72c25d94112760c468ee6a95cd9
Xeyal <xeyalbeylerov@gmail.com>
2020-07-27 02:37:27 +0400

    Maked new main page and controller

--- src/main/java/com/company/resumecv/controller/MainPageController.java
+++ src/main/java/com/company/resumecv/controller/MainPageController.java
@@ -25,26 +25,26 @@ public class MainPageController {
 
     @GetMapping
     public String userRegister(Model model, HttpSession session) {
-        UserForm userForm=(UserForm) session.getAttribute("user");
-        model.addAttribute("user",userForm);
+//        UserForm userForm=(UserForm) session.getAttribute("user");
+//        model.addAttribute("user",userForm);
         return "main";
     }
 
     //    Blank resume object
-    @ModelAttribute("user")
-    public UserForm getEmptyUserForm() {
-        UserForm userForm=new UserForm();
-        userForm.setId(1l);
-        userForm.setName("Test name");
-        userForm.setSurname("Test surname");
-        userForm.setEmail("test email");
-        userForm.setPassword("test password");
+//    @ModelAttribute("user")
+//    public UserForm getEmptyUserForm() {
+//        UserForm userForm=new UserForm();
+//        userForm.setId(1l);
+//        userForm.setName("Test name");
+//        userForm.setSurname("Test surname");
+//        userForm.setEmail("test email");
+//        userForm.setPassword("test password");
 
 //        ResumeForm resumeForm = resumeInitializer.resumeBuilder();
 //        ResumeForm resumeForm2 = resumeInitializer.resumeBuilder();
 //        ResumeForm resumeForm3 = resumeInitializer.resumeBuilder();
 //        ResumeForm resumeForm4 = resumeInitializer.resumeBuilder();
 //        userForm.setResumeList(Arrays.asList(resumeForm,resumeForm2,resumeForm3,resumeForm4));
-        return userForm;
-    }
+//        return userForm;
+//    }
 }
