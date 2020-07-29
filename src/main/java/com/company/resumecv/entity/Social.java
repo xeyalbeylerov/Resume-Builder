9ea4d2d6b6204f346c6523725c7bf3515a5b6dbc
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:22:53 +0400

    -Added implement Serializable and serialVersionUID

--- src/main/java/com/company/resumecv/entity/Social.java
+++ src/main/java/com/company/resumecv/entity/Social.java
@@ -4,15 +4,20 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "social")
 @Data
 @EqualsAndHashCode(of = "id")
-public class Social {
+public class Social implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String socialNetworkName;
     private String socialNetworkAddress;
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name="resume_id")
+//    private Resume resume;
 }
