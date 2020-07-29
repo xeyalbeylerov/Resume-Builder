46c6d86738293fee778063ee50e1a47b301a5fbf
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:25:26 +0400

    -Added  new fields: private String jobTitle; private String city; private String nationality; private String birthplace; private Date birthDate private UserForm userForm;

--- src/main/java/com/company/resumecv/form/ResumeForm.java
+++ src/main/java/com/company/resumecv/form/ResumeForm.java
@@ -1,9 +1,13 @@
 package com.company.resumecv.form;
 
 
+import com.company.resumecv.entity.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import java.util.Date;
 import java.util.List;
 
 
@@ -12,18 +16,26 @@ import java.util.List;
 public class ResumeForm {
     private Long id;
     private String resumeTitle;
+    private String jobTitle;
     private String name;
     private String surname;
     private String vocation;
     private String address;
+    private String city;
     private String country;
+    private String nationality;
+    private String birthplace;
+    private Date birthDate;
     private String phone;
     private String email;
     private String website;
-    private String aboutUs;
+    private String about;
     private String imageName;
-    private List<WorkExperienceForm> workExperience;
+    private List<EmploymentHistoryForm> employmentHistory;
     private List<EducationForm> educations;
     private List<SkillForm> skills;
     private List<SocialForm> socials;
+
+    private UserForm userForm;
+
 }
