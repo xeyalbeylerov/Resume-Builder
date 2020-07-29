2a6833ef02530059f9a5e8c52d5b3540408b8e7e
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:29:00 +0400

    -Test commented, it will uncommented

--- src/test/java/com/company/resumecv/ResumeCvApplicationTests.java
+++ src/test/java/com/company/resumecv/ResumeCvApplicationTests.java
@@ -1,13 +1,13 @@
-package com.company.resumecv;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ResumeCvApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.company.resumecv;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ResumeCvApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
