890f48b302c3abbb7b986420a627d66bc84f5dd7
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:22:00 +0400

    -Added implement Serializable and serialVersionUID -Added orphanRemoval = true for hard remove from db -changed hibernate @Cascade to JPA  cascade = CascadeType.ALL

--- src/main/java/com/company/resumecv/entity/Resume.java
+++ src/main/java/com/company/resumecv/entity/Resume.java
@@ -3,39 +3,64 @@ package com.company.resumecv.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+
 @Entity
 @Table(name = "resume")
 @Data
 @EqualsAndHashCode(of = "id")
-public class Resume {
+public class Resume implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String resumeTitle;
+    private String jobTitle;
+    @Column(length = 40)
     private String name;
+    @Column(length = 40)
     private String surname;
     private String vocation;
     private String address;
     private String country;
+    private String city;
+    private String nationality;
+    private String birthplace;
+    private Date birthDate;
     private String phone;
     private String email;
     private String website;
-    private String aboutUs;
+    private String about;
     private String imageName;
-    @OneToMany
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
-    private List<WorkExperience> workExperience;
-    @OneToMany
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+//    @ManyToOne
+//    @JoinColumn(name="user_id")
+//    private User user;
+
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<EmploymentHistory> employmentHistory;
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Education> educations;
-    @OneToMany
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Skill> skills;
-    @OneToMany
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Social> socials;
+
+//    @OneToMany(mappedBy="resume",orphanRemoval = true, cascade = CascadeType.ALL)
+//    @LazyCollection(LazyCollectionOption.FALSE)
+//    private List<EmploymentHistory> employmentHistory;
+//    @OneToMany(mappedBy="resume",orphanRemoval = true, cascade = CascadeType.ALL)
+//    @LazyCollection(LazyCollectionOption.FALSE)
+//    private List<Education> educations;
+//    @OneToMany(mappedBy="resume",orphanRemoval = true, cascade = CascadeType.ALL)
+//    @LazyCollection(LazyCollectionOption.FALSE)
+//    private List<Skill> skills;
+//    @OneToMany(mappedBy="resume",orphanRemoval = true, cascade = CascadeType.ALL)
+//    @LazyCollection(LazyCollectionOption.FALSE)
+//    private List<Social> socials;
 }
