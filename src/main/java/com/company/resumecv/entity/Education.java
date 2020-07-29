75f13f33ff5d6aaa1128dc7dcacb9d3232789321
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:22:25 +0400

    -Added implement Serializable and serialVersionUID

--- src/main/java/com/company/resumecv/entity/Education.java
+++ src/main/java/com/company/resumecv/entity/Education.java
@@ -1,19 +1,25 @@
 package com.company.resumecv.entity;
 
+import com.company.resumecv.form.ResumeForm;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "education")
 @Data
 @EqualsAndHashCode(of = "id")
-public class Education {
+public class Education implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String educationDate;
     private String educationHeader;
     private String educationBody;
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name="resume_id")
+//    private Resume resume;
 }
