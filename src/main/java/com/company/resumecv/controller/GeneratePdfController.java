923a6e2342b59e88bd0ab9241be0bb296ecb3669
Xeyal <xeyalbeylerov@gmail.com>
2020-07-30 01:31:42 +0400

    Class commented it will uncommented on future

--- src/main/java/com/company/resumecv/controller/GeneratePdfController.java
+++ src/main/java/com/company/resumecv/controller/GeneratePdfController.java
@@ -1,32 +1,32 @@
-package com.company.resumecv.controller;
-
-import com.company.resumecv.config.ResumeInitializerForTest;
-import com.company.resumecv.form.ResumeForm;
-import com.company.resumecv.service.Html2Pdf;
-import com.company.resumecv.service.ResumeService;
-import com.company.resumecv.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = "/generate")
-@RequiredArgsConstructor
-public class GeneratePdfController {
-    private final ResumeService resumeService;
-    private final Html2Pdf generatePdf;
-
-    @GetMapping("/{id}")
-    public String generatePdf(@PathVariable("id") Long id, Model model) {
-        ResumeForm resumeForm = resumeService.findById(id);
-        String inputUrl = "http://localhost:8080/resume/" + id;
-        String outputUrl = "TestPdf_" + id + ".pdf";
-        generatePdf.generatePDF(inputUrl);
-
-        model.addAttribute("resume", resumeForm);
-        return "resume";
-    }
-}
+//package com.company.resumecv.controller;
+//
+//import com.company.resumecv.config.ResumeInitializerForTest;
+//import com.company.resumecv.form.ResumeForm;
+//import com.company.resumecv.service.Html2Pdf;
+//import com.company.resumecv.service.ResumeService;
+//import com.company.resumecv.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "/generate")
+//@RequiredArgsConstructor
+//public class GeneratePdfController {
+//    private final ResumeService resumeService;
+//    private final Html2Pdf generatePdf;
+//
+//    @GetMapping("/{id}")
+//    public String generatePdf(@PathVariable("id") Long id, Model model) {
+//        ResumeForm resumeForm = resumeService.findById(id);
+//        String inputUrl = "http://localhost:8080/resume/" + id;
+//        String outputUrl = "TestPdf_" + id + ".pdf";
+//        generatePdf.generatePDF(inputUrl);
+//
+//        model.addAttribute("resume", resumeForm);
+//        return "resume";
+//    }
+//}
