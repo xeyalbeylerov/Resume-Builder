b6d4c5c8d8c2bc3ef3e94818ff094d2cdb3ee0be
Xeyal <xeyalbeylerov@gmail.com>
2020-08-02 19:06:29 +0400

    Added thymeleaf fragment method.But this is experimental

--- src/main/java/com/company/resumecv/controller/IndexController.java
+++ src/main/java/com/company/resumecv/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.company.resumecv.controller;
 
+import com.company.resumecv.config.ResumeInitializerForTest;
 import com.company.resumecv.form.*;
 import com.company.resumecv.service.ResumeService;
 import com.company.resumecv.service.UserService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.Arrays;
@@ -18,11 +20,36 @@ import java.util.Arrays;
 @RequestMapping(value = "/")
 @RequiredArgsConstructor
 public class IndexController {
+private final ResumeInitializerForTest initializer;
 
-
-    @GetMapping
-    public String builder(Model model) {
+    @GetMapping("/")
+    public String index(Model model) {
+        ResumeForm resumeForm = initializer.resumeBuilder();
+        resumeForm.setName(resumeForm.getName()+i);
+        model.addAttribute("resume",resumeForm);
         return "index";
     }
 
+    int i=0;
+    @GetMapping("/reload")
+    public ModelAndView reset(Model model) {
+        ResumeForm resumeForm = initializer.resumeBuilder();
+        resumeForm.setName("Reloaded "+i);
+        model.addAttribute("resume",resumeForm);
+        i++;
+        System.out.println(i);
+        return new ModelAndView("index :: header");
+//        return "index :: header";
+    }
+
+    @PostMapping("/reload2")
+    public ModelAndView reset2(Model model) {
+        ResumeForm resumeForm = initializer.resumeBuilder();
+        resumeForm.setName("Reloaded "+i);
+        model.addAttribute("resume",resumeForm);
+        i++;
+        System.out.println(i);
+        return new ModelAndView("index :: header");
+//        return "index :: header";
+    }
 }
